ce26d562b578dd7ec9b3ca30cb93ca59b2bcae96
caique-badaro <142758845+caique-badaro@users.noreply.github.com>
2023-11-20 20:17:06 -0300

    Add files via upload

--- js/main-info-details.js
+++ js/main-info-details.js
@@ -35,9 +35,48 @@ document.getElementById('heatmap-card--order-3').addEventListener('click', funct
     openPopup('popup-heatmap-view-3');
 });
 
+document.getElementById('matriz-card--order-1').addEventListener('click', function() {
+    openPopup('popup-matriz-view-1');
+});
+
+document.getElementById('matriz-card--order-2').addEventListener('click', function() {
+    openPopup('popup-matriz-view-2');
+});
+
+document.getElementById('matriz-card--order-3').addEventListener('click', function() {
+    openPopup('popup-matriz-view-3');
+});
+
 // Adicionando evento de clique ao overlay para fechar o popup
 document.getElementById('overlay').addEventListener('click', function() {
     closePopup('popup-heatmap-view-1');
     closePopup('popup-heatmap-view-2');
     closePopup('popup-heatmap-view-3');
+    closePopup('popup-matriz-view-1');
+    closePopup('popup-matriz-view-2');
+    closePopup('popup-matriz-view-3');
+});
+
+
+// FAQ Projeto >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Adiciona evento de clique a todas as divs com a classe faq-default
+const faqDefault = document.querySelectorAll('.faq-default');
+
+faqDefault.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    // Alterna as classes faq-default e faq-open
+    faq.classList.toggle('faq-default');
+    faq.classList.toggle('faq-open');
+  });
+});
+
+// Adiciona evento de clique a todas as divs com a classe faq-open
+const faqOpen = document.querySelectorAll('.faq-open');
+
+faqOpen.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    // Alterna as classes faq-default e faq-open
+    faq.classList.toggle('faq-default');
+    faq.classList.toggle('faq-open');
+  });
 });
